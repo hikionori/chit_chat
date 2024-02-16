92538dc4b7b7dc144fe812d7012ab6c4e6620683
Danila Verbinskiy <work.danv4656@gmail.com>
2024-02-16 09:22:17 +0200

    Add .env.local and files.sql

---
+++ supabase/migrations/20240216071542_files.sql
@@ -0,0 +1,19 @@
+create schema private;
+
+insert into storage.buckets(id, name) values ('files', 'files') on conflict do nothing;
+
+create policy "Authenticated can upload files" on storage.objects for insert to authenticated with check (
+    bucket_id = 'files' and owner = auth.uid()
+);
+
+create policy "Users can view their own files" on storage.objects for select to authenticated using (
+    bucket_id = 'files' and owner = auth.uid()
+);
+
+create policy "Users can delete their own files" on storage.objects for delete to authenticated using (
+    bucket_id = 'files' and owner = auth.uid()
+);
+
+create policy "Users can update their own files" on storage.objects for update to authenticated using (
+    bucket_id = 'files' and owner = auth.uid()
+);
